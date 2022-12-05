83f49bd46870b77d909e7cf34b84085bdcb7b602
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2022-12-05 22:44:19 +0800

    Update day4.mjs

--- day4.mjs
+++ day4.mjs
@@ -1,7 +1,19 @@
 
 import { readFileSync } from "node:fs";
 
-const poggies = readFileSync("day3.txt", { encoding: "utf-8" })
+const poggies = readFileSync("day4.txt", { encoding: "utf-8" })
     .replace(/\r/g, "")
     .trim()
     .split("\n");
+
+const part1 = () => {
+    poggies.map((x) => {
+        let [elf1, elf2] = x.split(",");
+        
+        console.log(elf1)
+
+        // console.log(x);
+    });
+}
+
+part1();
