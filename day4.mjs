d3a58b605799a2f4466c48999d878040ab0cf056
Matthew Chan <69852169+ZeInhumane@users.noreply.github.com>
2022-12-06 23:14:31 +0800

    Update day4.mjs

--- day4.mjs
+++ day4.mjs
@@ -7,13 +7,42 @@ const poggies = readFileSync("day4.txt", { encoding: "utf-8" })
     .split("\n");
 
 const part1 = () => {
+    let count = 0;
     poggies.map((x) => {
         let [elf1, elf2] = x.split(",");
-        
-        console.log(elf1)
+        let [a, b] = elf1.split("-");
+        let [c, d] = elf2.split("-");
 
-        // console.log(x);
+        a = parseInt(a);
+        b = parseInt(b);
+        c = parseInt(c);
+        d = parseInt(d);
+
+        if (a <= c && d <= b || c <= a && b <= d) {
+            count++
+        }
     });
+    console.log(count);
+}
+
+const part2 = () => {
+    let count = 0;
+    poggies.map((x) => {
+        let [elf1, elf2] = x.split(",");
+        let [a, b] = elf1.split("-");
+        let [c, d] = elf2.split("-");
+
+        a = parseInt(a);
+        b = parseInt(b);
+        c = parseInt(c);
+        d = parseInt(d);
+
+        if (b < c && d > a || d < a && b > c) {
+            count++;
+        }
+    });
+    console.log(1000 - count);
 }
 
 part1();
+part2();
